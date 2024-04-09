e288c9f382de8b2ff3765281b20b78f18bc69969
Nikita Anishenco <tanishenco91@gmail.com>
2024-04-09 10:42:56 +0300

    Добавлена крива я библиотека

--- Text_recognition_OCR.py
+++ Text_recognition_OCR.py
@@ -1,1 +1,19 @@
-print("")
+import pytesseract
+from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
+
+# Создание экземпляра Tkinter
+root = tk.Tk()
+root.withdraw()  # Скрытие окна Tkinter
+
+# Открытие диалога выбора файлов
+file_path = filedialog.askopenfilename()
+
+# Загрузка изображения
+img = Image.open(file_path)
+
+# Распознавание текста на изображении
+text = pytesseract.image_to_string(img)
+
+print(text)
